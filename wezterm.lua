d2a38f1b1b19a3e2ceb2da5ec250b1fcb896f5b8
Joel Cogen <joel@cogen.co>
2025-09-20 11:27:02 +0400

    wezterm

---
+++ wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.initial_cols = 120
+config.initial_rows = 50
+config.font_size = 14
+-- config.color_scheme = 'Gigavolt (base16)'
+config.enable_scroll_bar = true
+config.window_decorations = "RESIZE"
+
+config.keys = {
+  {
+    key = 'k',
+    mods = 'SUPER',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  },
+  {
+    key = 'd',
+    mods = 'SUPER',
+    action = act.SplitVertical,
+  },
+  {
+    key = 'D',
+    mods = 'SUPER',
+    action = act.SplitHorizontal,
+  },
+}
+
+return config
+
